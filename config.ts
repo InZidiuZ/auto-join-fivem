7a6e4113b4d7e4c6c0681dc5c84d2f34c089591e
Ben <inzidiuz@outlook.com>
2026-02-16 06:32:37 +0100

    fix(config): fix bool configs

--- config.ts
+++ config.ts
@@ -3,11 +3,20 @@ import { z } from "zod";
 
 dotenv.config({ quiet: true });
 
+const boolFromString = z.preprocess((value) => {
+	if (typeof value !== "string") return value;
+
+	if (value.toLowerCase() === "true") return true;
+	if (value.toLowerCase() === "false") return false;
+
+	return value;
+}, z.boolean());
+
 const configSchema = z.object({
 	SERVER_ENDPOINT: z.string(),
 	SERVER_IP: z.string(),
 
-	IS_SPECTATOR: z.boolean(),
+	IS_SPECTATOR: boolFromString.default(false),
 
 	LICENSE_IDENTIFIERS: z.string(),
 
